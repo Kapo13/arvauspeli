55a2d79d56c6898b4439581bda7ea8b0eec94197
Janne <janne.kaappola@student.hamk.fi>
2024-10-17 15:09:49 +0300

    Ohelman perusteet

--- arvauspeli/src/App.java
+++ arvauspeli/src/App.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
+    String oikeaNimi = "Janne";
+    String input;
+
+
+
+    System.out.println("Arvaa nimi?");
+    input = in.nextLine();
+
+    if (input.equals(oikeaNimi))
+    {
+    System.out.println("Oikein! Oikea nimi on " + oikeaNimi);
+    
+    }
+
+
+
     
 
     }
